17c9bf56656654f915849773687e5592a286f880
Igor Stoleski <igor.stoleski@gmx.de>
2023-06-24 22:56:17 +0200

    some logic added

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,7 @@ let questions = [
 ];
 
 let currentQuestion = 0;
+let numberQuestion = 1;
 
 function init(){
     document.getElementById('all-questions').innerHTML = questions.length;
@@ -25,21 +26,24 @@ function init(){
 }
 
 function showQuestion(){
-    let question = questions[currentQuestion];
-    document.getElementById('questiontext').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    if (currentQuestion >= questions.length) {
+        document.getElementById('end-screen').style = '';
+        document.getElementById('question-body').style = 'display: none';
+    }else{
+        let question = questions[currentQuestion];
+        document.getElementById('number-question').innerHTML = currentQuestion +1;
+        document.getElementById('questiontext').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
 }
 
 function answer(selection){
-    let question = questions[currentQuestion];
-    console.log('selected answer is, ', selection);
-    let selectedQuestionNumber = selection.slice(-1);
-    console.log('selectedQestionNumber is ', selectedQuestionNumber);
-
-    let idOfRightAnswer = `answer_3`;
+    let question = questions[currentQuestion];    
+    let selectedQuestionNumber = selection.slice(-1);    
+    let idOfRightAnswer = `answer_${question[`right_answer`]}`;
 
     if (selectedQuestionNumber == question['right_answer']) {        
         document.getElementById(selection).parentNode.classList.add('bg-success');
@@ -47,4 +51,23 @@ function answer(selection){
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
+    document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    showQuestion();
+    resetAnswer();
+}
+
+function resetAnswer(){
+    document.getElementById('next-button').disabled = true;
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
